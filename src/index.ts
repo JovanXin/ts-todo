2b65b690b8d81e4ce8c43611399a95b7af22090e
Jovan Xin <61258436+JovanXin@users.noreply.github.com>
2021-02-06 22:00:44 +1300

    Move todo types back into index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
+interface Todo {
+  id: number;
+  description: string;
+  place?: Place;
+  status: boolean;
+}
+
+interface CompletedTodo extends Todo {
+  status: true;
+}
+
+type Place = "home" | "work" | { custom: string };
+
 function toggleTodo(todo: Todo): Todo {
   return {
     id: todo.id,
